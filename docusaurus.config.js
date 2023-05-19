9356044d92e4ae919894b1e67ce450b6f4630a26
Martin Grabina <mgrabina@itba.edu.ar>
2023-05-19 11:25:46 -0300

    add styling

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,15 +74,15 @@ const config = {
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
+          // {
+          //   title: 'Docs',
+          //   items: [
+          //     {
+          //       label: 'Tutorial',
+          //       to: '/docs/intro',
+          //     },
+          //   ],
+          // },
           {
             title: 'Community',
             items: [
