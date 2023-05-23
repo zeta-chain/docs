67dadb7904b2783c9d61592a17762b34b2024dd1
Martin Grabina <mgrabina@itba.edu.ar>
2023-05-23 12:03:58 -0300

    add footer

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,19 +1,6 @@
-// import { Locale, translations } from "@zeta-shared/frontend-config/i18n";
-// import { ZetaAppsFooter } from "@zeta-shared/frontend-lib/src/components/Footer/components/ZetaAppsFooter";
-// import { ZetaApp } from "@zeta-shared/frontend-lib/src/constants";
-// import { deepCloneSerializableValue } from "@zeta-shared/frontend-lib/src/utils";
-// import { merge } from "lodash-es";
-// import React, { useMemo } from "react";
+import { ZetaAppsFooter } from "./ZetaAppsFooter";
+import React from "react";
 
-// const getTranslations = (locale?: Locale) =>
-//   locale && locale !== "en"
-//     ? merge(deepCloneSerializableValue(translations.en.copy), translations[locale].copy)
-//     : translations.en.copy;
-
-// eslint-disable-next-line import/no-default-export
 export default function Footer() {
-  // const copy = useMemo(() => getTranslations(), []);
-
-  // return <ZetaAppsFooter app={ZetaApp.Docs} copy={copy} />;
-  return null;
+  return <ZetaAppsFooter />;
 }
