8f0f989a2d38a9884a7b0d749abe2f6c9a1b956c
Manuel Figueira <manuel.figueira.9@gmail.com>
2023-06-14 15:25:44 -0300

    fix

--- src/theme/Navbar/MobileSidebar/index.js
+++ src/theme/Navbar/MobileSidebar/index.js
@@ -1,9 +1,16 @@
-import { useLockBodyScroll, useNavbarMobileSidebar } from "@docusaurus/theme-common/internal";
+import {
+  useLockBodyScroll,
+  useNavbarMobileSidebar,
+} from "@docusaurus/theme-common/internal";
 import { DocSidebarDesktop } from "../../DocSidebar";
+
+import React from "react";
 
 // another example of docusaurus being awful, the build breaks when using this hook if we don't check for window
 const useDocsSidebar =
-  typeof window !== "undefined" ? require("@docusaurus/theme-common/internal").useDocsSidebar : () => ({ items: [] });
+  typeof window !== "undefined"
+    ? require("@docusaurus/theme-common/internal").useDocsSidebar
+    : () => ({ items: [] });
 
 export default function NavbarMobileSidebar() {
   const mobileSidebar = useNavbarMobileSidebar();
@@ -17,7 +24,10 @@ export default function NavbarMobileSidebar() {
 
   return (
     <div className="w-full absolute m-0 top-[60px] p-0 left-0 overflow-y-scroll h-screen bg-white dark:bg-black">
-      <DocSidebarDesktop path={window?.location?.pathname || ""} sidebar={docsSidebar.items} />
+      <DocSidebarDesktop
+        path={window?.location?.pathname || ""}
+        sidebar={docsSidebar.items}
+      />
     </div>
   );
 }
