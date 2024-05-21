f9391b270adb7f44728aaedbb05f62020ae3f020
Manuel Figueira <manuel.figueira.9@gmail.com>
2024-05-21 16:02:35 -0300

    update styles

--- src/components/About/components/WhatIsZetaChain.tsx
+++ src/components/About/components/WhatIsZetaChain.tsx
@@ -8,17 +8,17 @@ export const WhatIsZetaChain: React.FC = () => {
       <ArticleNavigationTitle title="What is ZetaChain?" description="An overview" />
 
       <div className="flex flex-col gap-8">
-        <div className="flex flex-col sm:flex-row gap-4 sm:gap-8 text-base leading-[160%] font-normal text-grey-400 dark:text-grey-300">
-          <p>
-            ZetaChain is the foundational, public blockchain that enables omnichain, generic smart contracts and
-            messaging between any blockchain. It solves the problems of “cross-chain” and “multi-chain” and aims to open
-            the crypto and global financial ecosystem to anyone.
+        <div className="grid grid-cols-10 gap-5 md:gap-8 text-base leading-[160%] font-normal text-grey-400 dark:text-grey-300">
+          <p className="col-span-10 md:col-span-5">
+            ZetaChain is the foundational, public blockchain that enables omnichain smart contracts and messaging
+            between any blockchain. It solves the problems of “cross-chain” and “multi-chain” and aims to open the
+            crypto and global financial ecosystem to anyone.
           </p>
 
-          <p>
+          <p className="col-span-10 md:col-span-5">
             ZetaChain envisions and supports a truly fluid, multi-chain crypto ecosystem, where users and developers can
             move between and appreciate the benefits of any blockchain: payments, DeFi, liquidity, games, art, social
-            graphs, performance, security, privacy, and so on.
+            graphs, performance, security, privacy, and more.
           </p>
         </div>
 
@@ -29,7 +29,7 @@ export const WhatIsZetaChain: React.FC = () => {
             </div>
 
             <div>
-              <p className="text-sm text-grey-400 dark:text-grey-300">
+              <p className="text-sm mb-0.5 text-grey-400 dark:text-grey-300">
                 Get a closer look at ZetaChain's background and architecture
               </p>
 
--- src/components/Home/components/HomeHeroDescription.tsx
+++ src/components/Home/components/HomeHeroDescription.tsx
@@ -6,13 +6,13 @@ import { PrimaryLink } from "../../shared/components/Link";
 export const HomeHeroDescription: React.FC = () => {
   return (
     <div className="flex flex-col gap-8 sm:gap-10">
-      <div className="flex flex-col sm:flex-row gap-4 sm:gap-8">
-        <p>
+      <div className="grid grid-cols-10 gap-5 md:gap-8 text-base leading-[160%] font-normal text-grey-400 dark:text-grey-300">
+        <p className="col-span-10 md:col-span-5">
           ZetaChain is the only decentralized blockchain and smart contract platform built for omnichain
           interoperability.
         </p>
 
-        <p>
+        <p className="col-span-10 md:col-span-5">
           A truly fluid, multi-chain crypto ecosystem, where users and developers can use and appreciate the benefits of
           any blockchain.
         </p>
