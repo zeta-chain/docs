3de62594ba910cb6d9d46dd56928c4970369ff60
Manuel Figueira <manuel.figueira.9@gmail.com>
2024-05-20 23:38:34 -0300

    add more article icons

--- src/components/shared/components/Icons/components/IconArticle/components/IconArticleRandom.tsx
+++ src/components/shared/components/Icons/components/IconArticle/components/IconArticleRandom.tsx
@@ -3,6 +3,8 @@ import { useEffect, useMemo, useState } from "react";
 import { IconArticle001 } from "./IconArticle001";
 import { IconArticle002 } from "./IconArticle002";
 import { IconArticle003 } from "./IconArticle003";
+import { IconArticle004 } from "./IconArticle004";
+import { IconArticle005 } from "./IconArticle005";
 import { IconArticle006 } from "./IconArticle006";
 import { IconArticle007 } from "./IconArticle007";
 import { IconArticle011 } from "./IconArticle011";
@@ -11,6 +13,8 @@ import { IconArticle013 } from "./IconArticle013";
 import { IconArticle026 } from "./IconArticle026";
 import { IconArticle027 } from "./IconArticle027";
 import { IconArticle028 } from "./IconArticle028";
+import { IconArticle029 } from "./IconArticle029";
+import { IconArticle030 } from "./IconArticle030";
 import { IconArticle031 } from "./IconArticle031";
 import { IconArticle032 } from "./IconArticle032";
 import { IconArticle036 } from "./IconArticle036";
@@ -19,6 +23,8 @@ import { IconArticle038 } from "./IconArticle038";
 import { IconArticle051 } from "./IconArticle051";
 import { IconArticle052 } from "./IconArticle052";
 import { IconArticle053 } from "./IconArticle053";
+import { IconArticle054 } from "./IconArticle054";
+import { IconArticle055 } from "./IconArticle055";
 import { IconArticle056 } from "./IconArticle056";
 import { IconArticle057 } from "./IconArticle057";
 import { IconArticle061 } from "./IconArticle061";
@@ -27,6 +33,8 @@ import { IconArticle063 } from "./IconArticle063";
 import { IconArticle076 } from "./IconArticle076";
 import { IconArticle077 } from "./IconArticle077";
 import { IconArticle078 } from "./IconArticle078";
+import { IconArticle079 } from "./IconArticle079";
+import { IconArticle080 } from "./IconArticle080";
 import { IconArticle081 } from "./IconArticle081";
 import { IconArticle082 } from "./IconArticle082";
 import { IconArticle086 } from "./IconArticle086";
@@ -35,6 +43,8 @@ import { IconArticle088 } from "./IconArticle088";
 import { IconArticle101 } from "./IconArticle101";
 import { IconArticle102 } from "./IconArticle102";
 import { IconArticle103 } from "./IconArticle103";
+import { IconArticle104 } from "./IconArticle104";
+import { IconArticle105 } from "./IconArticle105";
 import { IconArticle106 } from "./IconArticle106";
 import { IconArticle107 } from "./IconArticle107";
 import { IconArticle111 } from "./IconArticle111";
@@ -45,6 +55,8 @@ const articleIcons = [
   <IconArticle001 />,
   <IconArticle002 />,
   <IconArticle003 />,
+  <IconArticle004 />,
+  <IconArticle005 />,
   <IconArticle006 />,
   <IconArticle007 />,
   <IconArticle011 />,
@@ -53,6 +65,8 @@ const articleIcons = [
   <IconArticle026 />,
   <IconArticle027 />,
   <IconArticle028 />,
+  <IconArticle029 />,
+  <IconArticle030 />,
   <IconArticle031 />,
   <IconArticle032 />,
   <IconArticle036 />,
@@ -61,6 +75,8 @@ const articleIcons = [
   <IconArticle051 />,
   <IconArticle052 />,
   <IconArticle053 />,
+  <IconArticle054 />,
+  <IconArticle055 />,
   <IconArticle056 />,
   <IconArticle057 />,
   <IconArticle061 />,
@@ -69,6 +85,8 @@ const articleIcons = [
   <IconArticle076 />,
   <IconArticle077 />,
   <IconArticle078 />,
+  <IconArticle079 />,
+  <IconArticle080 />,
   <IconArticle081 />,
   <IconArticle082 />,
   <IconArticle086 />,
@@ -77,6 +95,8 @@ const articleIcons = [
   <IconArticle101 />,
   <IconArticle102 />,
   <IconArticle103 />,
+  <IconArticle104 />,
+  <IconArticle105 />,
   <IconArticle106 />,
   <IconArticle107 />,
   <IconArticle111 />,
@@ -84,13 +104,11 @@ const articleIcons = [
   <IconArticle113 />,
 ];
 
-const ICON_COUNT = articleIcons.length;
-
 export const IconArticleRandom: React.FC = () => {
   const [isMounted, setIsMounted] = useState(false);
   useEffect(() => setIsMounted(true), []);
 
-  const randomIndex = useMemo(() => Math.floor(Math.random() * ICON_COUNT), []);
+  const randomIndex = useMemo(() => Math.floor(Math.random() * articleIcons.length), []);
 
   if (!isMounted) return null;
 
