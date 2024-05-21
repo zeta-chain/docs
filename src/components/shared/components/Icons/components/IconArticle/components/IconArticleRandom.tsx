d6b891fffe98688759407ce4b87ad0650ee4ae56
Manuel Figueira <manuel.figueira.9@gmail.com>
2024-05-21 00:04:54 -0300

    add more article icons

--- src/components/shared/components/Icons/components/IconArticle/components/IconArticleRandom.tsx
+++ src/components/shared/components/Icons/components/IconArticle/components/IconArticleRandom.tsx
@@ -7,6 +7,9 @@ import { IconArticle004 } from "./IconArticle004";
 import { IconArticle005 } from "./IconArticle005";
 import { IconArticle006 } from "./IconArticle006";
 import { IconArticle007 } from "./IconArticle007";
+import { IconArticle008 } from "./IconArticle008";
+import { IconArticle009 } from "./IconArticle009";
+import { IconArticle010 } from "./IconArticle010";
 import { IconArticle011 } from "./IconArticle011";
 import { IconArticle012 } from "./IconArticle012";
 import { IconArticle013 } from "./IconArticle013";
@@ -17,6 +20,9 @@ import { IconArticle029 } from "./IconArticle029";
 import { IconArticle030 } from "./IconArticle030";
 import { IconArticle031 } from "./IconArticle031";
 import { IconArticle032 } from "./IconArticle032";
+import { IconArticle033 } from "./IconArticle033";
+import { IconArticle034 } from "./IconArticle034";
+import { IconArticle035 } from "./IconArticle035";
 import { IconArticle036 } from "./IconArticle036";
 import { IconArticle037 } from "./IconArticle037";
 import { IconArticle038 } from "./IconArticle038";
@@ -27,6 +33,9 @@ import { IconArticle054 } from "./IconArticle054";
 import { IconArticle055 } from "./IconArticle055";
 import { IconArticle056 } from "./IconArticle056";
 import { IconArticle057 } from "./IconArticle057";
+import { IconArticle058 } from "./IconArticle058";
+import { IconArticle059 } from "./IconArticle059";
+import { IconArticle060 } from "./IconArticle060";
 import { IconArticle061 } from "./IconArticle061";
 import { IconArticle062 } from "./IconArticle062";
 import { IconArticle063 } from "./IconArticle063";
@@ -37,6 +46,9 @@ import { IconArticle079 } from "./IconArticle079";
 import { IconArticle080 } from "./IconArticle080";
 import { IconArticle081 } from "./IconArticle081";
 import { IconArticle082 } from "./IconArticle082";
+import { IconArticle083 } from "./IconArticle083";
+import { IconArticle084 } from "./IconArticle084";
+import { IconArticle085 } from "./IconArticle085";
 import { IconArticle086 } from "./IconArticle086";
 import { IconArticle087 } from "./IconArticle087";
 import { IconArticle088 } from "./IconArticle088";
@@ -47,6 +59,9 @@ import { IconArticle104 } from "./IconArticle104";
 import { IconArticle105 } from "./IconArticle105";
 import { IconArticle106 } from "./IconArticle106";
 import { IconArticle107 } from "./IconArticle107";
+import { IconArticle108 } from "./IconArticle108";
+import { IconArticle109 } from "./IconArticle109";
+import { IconArticle110 } from "./IconArticle110";
 import { IconArticle111 } from "./IconArticle111";
 import { IconArticle112 } from "./IconArticle112";
 import { IconArticle113 } from "./IconArticle113";
@@ -59,6 +74,9 @@ const articleIcons = [
   <IconArticle005 />,
   <IconArticle006 />,
   <IconArticle007 />,
+  <IconArticle008 />,
+  <IconArticle009 />,
+  <IconArticle010 />,
   <IconArticle011 />,
   <IconArticle012 />,
   <IconArticle013 />,
@@ -69,6 +87,9 @@ const articleIcons = [
   <IconArticle030 />,
   <IconArticle031 />,
   <IconArticle032 />,
+  <IconArticle033 />,
+  <IconArticle034 />,
+  <IconArticle035 />,
   <IconArticle036 />,
   <IconArticle037 />,
   <IconArticle038 />,
@@ -79,6 +100,9 @@ const articleIcons = [
   <IconArticle055 />,
   <IconArticle056 />,
   <IconArticle057 />,
+  <IconArticle058 />,
+  <IconArticle059 />,
+  <IconArticle060 />,
   <IconArticle061 />,
   <IconArticle062 />,
   <IconArticle063 />,
@@ -89,6 +113,9 @@ const articleIcons = [
   <IconArticle080 />,
   <IconArticle081 />,
   <IconArticle082 />,
+  <IconArticle083 />,
+  <IconArticle084 />,
+  <IconArticle085 />,
   <IconArticle086 />,
   <IconArticle087 />,
   <IconArticle088 />,
@@ -99,6 +126,9 @@ const articleIcons = [
   <IconArticle105 />,
   <IconArticle106 />,
   <IconArticle107 />,
+  <IconArticle108 />,
+  <IconArticle109 />,
+  <IconArticle110 />,
   <IconArticle111 />,
   <IconArticle112 />,
   <IconArticle113 />,
