440d1bc8e762e7213da0443190168aceb1af1ef7
Manuel Figueira <manuel.figueira.9@gmail.com>
2024-05-27 13:27:53 -0300

    extend xl screen width

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,9 @@ const tailwindTheme = require("@zetachain/ui-toolkit/theme/tailwind.config");
 
 tailwindTheme.content = ["./src/**/*.{ts,tsx,js,jsx,md,mdx}"];
 tailwindTheme.plugins = [require("@tailwindcss/line-clamp")];
+tailwindTheme.theme.screens = {
+  ...tailwindTheme.theme.screens,
+  xl: "1512px",
+};
 
 module.exports = tailwindTheme;
