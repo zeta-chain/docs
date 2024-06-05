e8e2d21ae498aaa432589393860d2da6e8c112a0
Manuel Figueira <manuel.figueira.9@gmail.com>
2024-06-05 11:28:32 -0300

    swap testnet for mainnet tab order

--- src/components/shared/components/Tabs/Tabs.constants.ts
+++ src/components/shared/components/Tabs/Tabs.constants.ts
@@ -4,11 +4,11 @@ export const networkTypeTabs: {
   label: string;
   networkType: NetworkType;
 }[] = [
-  { label: "Testnet", networkType: "testnet" },
   { label: "Mainnet Beta", networkType: "mainnet" },
+  { label: "Testnet", networkType: "testnet" },
 ];
 
 export const rpcByNetworkType: Record<NetworkType, string> = {
-  testnet: "https://zetachain-athens.blockpi.network/lcd/v1/public",
   mainnet: "https://zetachain.blockpi.network/lcd/v1/public",
+  testnet: "https://zetachain-athens.blockpi.network/lcd/v1/public",
 };
