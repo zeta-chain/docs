f1857d4c9240fad5970d1a26f8865d49b093bc9c
Manuel Figueira <manuel.figueira.9@gmail.com>
2024-06-08 08:51:41 +0900

    revert commit

--- src/lib/tracking.ts
+++ src/lib/tracking.ts
@@ -1,21 +1,12 @@
-/* eslint-disable no-console */
 import { AnalyticsBrowser } from "@segment/analytics-next";
 
 import { environment } from "../env.cjs";
-import { isClient, isNodeEnvProd, isVercelPreview, isVercelProd } from "./app.constants";
+import { isClient, isNodeEnvProd, isVercelProd } from "./app.constants";
 
 const samplingRate = 0.5;
 const shouldLoadAnalytics = Math.random() < samplingRate;
 
 const isClientProd = isNodeEnvProd && isVercelProd && isClient;
-
-console.log("isNodeEnvProd :>> ", isNodeEnvProd);
-console.log("process.env.NODE_ENV :>> ", process.env.NODE_ENV);
-console.log("isVercelProd :>> ", isVercelProd);
-console.log("isVercelPreview :>> ", isVercelPreview);
-console.log("environment.NEXT_PUBLIC_VERCEL_ENV :>> ", environment.NEXT_PUBLIC_VERCEL_ENV);
-console.log("isClient :>> ", isClient);
-console.log("isClientProd :>> ", isClientProd);
 
 export enum AnalyticsTrackEvents {
   DocsLinkClicked = "docs link clicked",
@@ -26,8 +17,6 @@ export enum AnalyticsProperties {}
 type AnalyticsPropertiesRecord = Partial<Record<AnalyticsProperties, string | null>>;
 
 const segmentKey = environment.NEXT_PUBLIC_SEGMENT_KEY || "";
-
-console.log("segmentKey :>> ", segmentKey);
 
 const analytics =
   isClientProd && shouldLoadAnalytics && segmentKey
