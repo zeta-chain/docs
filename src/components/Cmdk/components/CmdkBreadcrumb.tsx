cd9078e0a635ee9c298fc34c174fe28d5f3c3d13
Lucas <ljanon@gmail.com>
2024-07-31 11:49:46 -0300

    Add cmdk + ZetaAI (#406)

---
+++ src/components/Cmdk/components/CmdkBreadcrumb.tsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+
+interface CommandShortcutProps {
+  className?: string;
+  children?: React.ReactNode;
+  onClick?: () => void;
+  type?: "default" | "breadcrumb";
+}
+
+export const CmdkBreadcrumb = ({ className, children, onClick, type = "default" }: CommandShortcutProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={clsx(
+        "cursor-default px-3 py-0.5 rounded text-xs [&:not(:last-child)]:hover:cursor-pointer",
+        "justify-end",
+        type === "breadcrumb"
+          ? "text-foreground-muted"
+          : "bg-overlay-hover text-foreground-muted [&:not(:last-child)]:hover:bg-selection last:bg-selection last:text-foreground-muted",
+        className
+      )}
+    >
+      {children}
+    </button>
+  );
+};
