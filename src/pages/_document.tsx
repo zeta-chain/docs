aba2ad7ab1996e1627df9867cb578e4a5da4c15c
GMaiolo <guille.maiolo@gmail.com>
2024-08-26 13:39:26 -0300

    Update document tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,48 @@
+import createEmotionServer from "@emotion/server/create-instance";
+import Document, { DocumentContext, Head, Html, Main, NextScript } from "next/document";
+import React from "react";
+
+import { createEmotionCache } from "~/lib/helpers/createEmotionCache";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const originalRenderPage = ctx.renderPage;
+
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) => (props) => <App emotionCache={cache} {...props} />,
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style
+        data-emotion={`${style.key} ${style.ids.join(" ")}`}
+        key={style.key}
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    ));
+
+    return {
+      ...initialProps,
+      styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
+    };
+  }
+
+  render() {
+    return (
+      <Html className="dark">
+        <Head />
+
+        <body className="no-scrollbar">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
