0374af540fdca306c8fbfb34c8ec1e6e8543d8e7
Manuel Figueira <manuel.figueira.9@gmail.com>
2025-06-30 16:05:29 -0300

    update graphql

--- src/components/Home/Home.graphql.contentful.ts
+++ src/components/Home/Home.graphql.contentful.ts
@@ -1,50 +1,36 @@
 import { gql } from "graphql-request";
 
-export const FeaturedEcosystemApp = gql`
-  fragment FeaturedEcosystemApp on EcosystemProject {
-    sys {
-      id
-    }
-    name
-    description
-    image {
-      url
-    }
-    link
-    textColor
-
-    categoryCollection {
-      items {
-        sys {
-          id
-        }
-      }
-    }
-
-    isFeatured
-    featuredAppOrder
-    featuredCardBackgroundImage {
-      url
-    }
-    featuredCardBorder
-    featuredCardLogoBorder
-  }
-`;
-
 export const GetFeaturedEcosystemApps = gql`
   query GetFeaturedEcosystemApps {
     ecosystemProjectCollection(where: { isFeatured: true }, order: featuredAppOrder_ASC, limit: 5) {
       items {
-        ...FeaturedEcosystemApp
-      }
-    }
-  }
-`;
+        sys {
+          id
+        }
+        name
+        description
+        image {
+          url
+        }
+        link
+        textColor
 
-export const GetEcosystemAppsCount = gql`
-  query GetEcosystemAppsCount {
-    ecosystemProjectCollection {
-      total
+        categoryCollection {
+          items {
+            sys {
+              id
+            }
+          }
+        }
+
+        isFeatured
+        featuredAppOrder
+        featuredCardBackgroundImage {
+          url
+        }
+        featuredCardBorder
+        featuredCardLogoBorder
+      }
     }
   }
 `;
