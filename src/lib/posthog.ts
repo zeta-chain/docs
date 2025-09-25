43c3f21c5b7e9d32d0112ed69f32f04307d096b0
Hernan Clich <hernan.g.clich@gmail.com>
2025-09-25 12:12:57 -0300

    Singleton pattern to node client

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -1,11 +1,15 @@
 import { PostHog } from "posthog-node";
 
+let posthogClient: PostHog | null = null;
+
 // NOTE: This is a Node.js client, so you can use it for sending events from the server side to PostHog.
 export default function PostHogClient() {
-  const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    flushAt: 1,
-    flushInterval: 0,
-  });
+  if (!posthogClient) {
+    posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      flushAt: 1,
+      flushInterval: 0,
+    });
+  }
   return posthogClient;
 }
